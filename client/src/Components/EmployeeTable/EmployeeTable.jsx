85d90ee66623c24818cd0a3c42224a6bb9101d7a
Paul <paulcosteacristian@yahoo.com>
2023-04-19 14:07:35 +0300

    task 2

--- client/src/Components/EmployeeTable/EmployeeTable.jsx
+++ client/src/Components/EmployeeTable/EmployeeTable.jsx
@@ -1,16 +1,63 @@
 import { Link } from "react-router-dom";
 import "./EmployeeTable.css";
 import { useAtom } from "jotai";
+import { useState, useEffect } from "react";
 import state from "../../Pages/Atom"
 
 const EmployeeTable = ({ employees, onDelete }) => {
 
-  console.log(employees);
 
   const [filter, setFilter] = useAtom(state.filter)
+  const [sort, setSort] = useState(0);
+
+  const sortByFirstName = () => {
+    employees.map(employee => employee.name = employee.name.split(" "));
+    employees.sort((a,b) => a.name[0] > b.name[0] ?  1 : -1)
+    employees.map(employee => employee.name = employee.name.join(" "));
+    setSort(sort*1 + 1);
+  }
+
+  const sortByLastName = () => {
+    employees.map(employee => employee.name = employee.name.split(" "));
+    employees.sort((a,b) => a.name[a.name.length - 1] > b.name[b.name.length - 1] ?  1 : -1)
+    employees.map(employee => employee.name = employee.name.join(" "));
+    setSort(sort*1 + 1);
+  }
+
+  const sortByMiddleName = () => {
+    employees.map(employee => {
+      employee.name = employee.name.split(" ")
+      if(employee.name.length === 2){
+        employee.name.splice(1, 0, "z");
+      }
+    });
+    employees.sort((a,b) => a.name[1] > b.name[1] ?  1 : -1);
+    employees.map(employee => {
+      if(employee.name[1] === "z"){
+        employee.name.splice(1, 1);
+      }
+      employee.name = employee.name.join(" ")
+    });
+    setSort(sort*1 + 1);
+  }
+
+  const sortByPosition = () => {
+    employees.sort((a,b) => a.position > b.position ?  1 : -1)
+    setSort(sort*1 + 1);
+  }
+
+  const sortByLevel = () => { 
+    employees.sort((a,b) => a.level > b.level ?  1 : -1)
+    setSort(sort*1 + 1);
+  }
 
   return (
     <div className="EmployeeTable">
+        <button onClick={sortByFirstName} id="sortButton">Sort by First Name</button>
+        <button onClick={sortByLastName} id="sortButton">Sort by Last Name</button>
+        <button onClick={sortByMiddleName} id="sortButton">Sort by Middle Name</button>
+        <button onClick={sortByPosition} id="sortButton">Sort by Position</button>
+        <button onClick={sortByLevel} id="sortButton">Sort by Level</button>
       <table>
         <thead>
           <tr>
@@ -22,7 +69,7 @@ const EmployeeTable = ({ employees, onDelete }) => {
         </thead>
         <tbody>
           {
-          employees && employees.map(employee => {   
+           employees.map(employee => {   
             if(employee.level.includes(filter) || employee.position.includes(filter)){
               return (
                 <tr key={employee._id}>
